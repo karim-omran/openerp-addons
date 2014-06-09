3ba52c80d5d1080034d1a5dfc1b4a7154489d11f
roufyda <roufyda@roufyda-HP-G62-Notebook-PC.(none)>
2014-06-09 16:17:33 -0400

    roufyda

---
+++ attendance_analysis/__openerp__.py
@@ -0,0 +1,13 @@
+{
+	
+	'name':'attendance_analysis',
+	'version':'1.0',
+	'description':'module for Attendance analysis',
+	'category':'companies',
+	'author':'roufyda',
+	'depends':['base',],
+	'data':['view/attendance_analysis_view.xml'],
+	'installable':'true',
+
+
+}
